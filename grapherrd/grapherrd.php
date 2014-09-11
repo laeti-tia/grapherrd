639859227661e9976ca20f210e01bf6783592915
Antoine <antoine@cassiopea.org>
2014-09-11 13:50:21 +0200

    Adding line wrap in footer before date.

--- grapherrd/grapherrd.php
+++ grapherrd/grapherrd.php
@@ -75,7 +75,7 @@ $endtime = $mtime;
 $totaltime = ($endtime - $starttime);
 print "<a href=\"http://validator.w3.org/check/referer\"><img style=\"border:0;float:left;\" src=\"images/valid-xhtml10.png\" alt=\"Valid XHTML 1.0!\" /></a>\n";
 print "<a href=\"http://jigsaw.w3.org/css-validator/check/referer\"><img style=\"border:0;float:right;\" src=\"images/vcss.png\" alt=\"Valid CSS!\" /></a>\n";
-printf ("<p>%s<br />\nPage created in %.3f seconds by <em>grapherrd</em> %s on %s.</p>\n", $cfg->footer, $totaltime, $cfg->version, date('l jS \of F Y H:i:s'));
+printf ("<p>%s<br />Page created in %.3f seconds by <em>grapherrd</em> %s<br />on %s.</p>\n", $cfg->footer, $totaltime, $cfg->version, date('l jS \of F Y H:i:s'));
 print "</div>";
 
 ?>
